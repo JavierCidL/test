7c0905876334229fc141523e91f4e1a1c3ed3949
alumno <alumno@PC-XX>
2019-02-05 14:15:03 +0100

    Segundo commit

---
+++ proyectoGitHub/src/proyectoGitHub/NuevaClase.java
@@ -0,0 +1,10 @@
+package proyectoGitHub;
+
+public class NuevaClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- proyectoGitHub/src/proyectoGitHub/PruebaClase.java
+++ proyectoGitHub/src/proyectoGitHub/PruebaClase.java
@@ -3,8 +3,10 @@ package proyectoGitHub;
 public class PruebaClase {
 
 	public static void main(String[] args) {
+		int x=0;
 		
-
+		if (x==0) 
+		 System.out.println("X es: "+x);
 	}
 
 }
